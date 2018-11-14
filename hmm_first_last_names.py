1648ccdab3a045128904a7d7000819d40a79eb29
Alberto Lezcano <lla11358@phobos.icane.es>
2018-11-14 10:23:17 +0100

    Refactor code to separate token treatment into functions

--- hmm_first_last_names.py
+++ hmm_first_last_names.py
@@ -13,166 +13,169 @@ Considered tokens:
     Dot (.) as abbreviation is considered part of the name.
 """
 
-from pomegranate import *
+from pomegranate import HiddenMarkovModel, DiscreteDistribution, State
 import re
 import math
 
+# Pattern defining the token separators
+separators = '[^-, ]+'
+# Raw data files
+first_name_file = './data/raw_first_name.txt'
+last_name_1_file = './data/raw_last_name_1.txt'
+last_name_2_file = './data/raw_last_name_2.txt'
 
-def discrete_distributions():
-    first_name_file = './data/raw_first_name.txt'
-    last_name_1_file = './data/raw_last_name_1.txt'
-    last_name_2_file = './data/raw_last_name_2.txt'
-    first_name_dist = {}
-    last_name_1_dist = {}
-    last_name_2_dist = {}
-    pattern = '[^-, ]+'
 
-    # Calculate probabilities for first name
-    token_count = 0
-    for line in open(first_name_file):
+def extract_tokens(file_name, pattern):
+    """
+    Extract all tokens found in a raw data file.
+
+    Parameters:
+        file_name (str): path of the file containing raw data
+        pattern (str): regular expression used to identify separators
+    Returns:
+        tokens (list)
+
+    """
+    tokens = []
+    for line in open(file_name):
         line = line.strip('\n').strip()
         # List of tokens from each line
-        tokens = re.findall(pattern, line)
-        # Add tokens to dictionary
-        for token in tokens:
-            token_count += 1
-            if token not in first_name_dist:
-                first_name_dist[token] = 1
-            else:
-                first_name_dist[token] += 1
-    # Add tokens from other data files
-    for line in open(last_name_1_file):
-        line = line.strip('\n').strip()
-        tokens = re.findall(pattern, line)
-        for token in tokens:
-            if token not in first_name_dist:
-                first_name_dist[token] = 0
-    for line in open(last_name_2_file):
-        line = line.strip('\n').strip()
-        tokens = re.findall(pattern, line)
-        for token in tokens:
-            if token not in first_name_dist:
-                first_name_dist[token] = 0
-    # Calculate probabilities
-    for key, value in first_name_dist.iteritems():
-        first_name_dist[key] = float(value) / float(token_count)
+        line_tokens = re.findall(pattern, line)
+        for line_token in line_tokens:
+            tokens.append(line_token)
+    return tokens
 
-    # Calculate probabilities for last name 1
+
+def probability_distribution(tokens):
+    """
+    Calculate the probabilities for each token in a given list.
+
+    Parameters:
+        tokens (list): list containing tokens
+
+    Returns:
+        prob_dist (dict): dictionary of token-probability pairs
+
+    """
     token_count = 0
-    for line in open(last_name_1_file):
-        line = line.strip('\n').strip()
-        # List of tokens from each line
-        tokens = re.findall(pattern, line)
-        # Add tokens to dictionary
-        for token in tokens:
-            token_count += 1
-            if token not in last_name_1_dist:
-                last_name_1_dist[token] = 1
-            else:
-                last_name_1_dist[token] += 1
-    # Add tokens from other data files
-    for line in open(first_name_file):
-        line = line.strip('\n').strip()
-        tokens = re.findall(pattern, line)
-        for token in tokens:
-            if token not in last_name_1_dist:
-                last_name_1_dist[token] = 0
-    for line in open(last_name_2_file):
-        line = line.strip('\n').strip()
-        tokens = re.findall(pattern, line)
-        for token in tokens:
-            if token not in last_name_1_dist:
-                last_name_1_dist[token] = 0
+    prob_dist = {}
+    # Frequencies of each token
+    for token in tokens:
+        token_count += 1
+        if token not in prob_dist:
+            prob_dist[token] = 1
+        else:
+            prob_dist[token] += 1
     # Calculate probabilities
-    for key, value in last_name_1_dist.iteritems():
-        last_name_1_dist[key] = float(value) / float(token_count)
-
-    # Calculate probabilities for last name 2
-    token_count = 0
-    for line in open(last_name_2_file):
-        line = line.strip('\n').strip()
-        # List of tokens from each line
-        tokens = re.findall(pattern, line)
-        # Add tokens to dictionary
-        for token in tokens:
-            token_count += 1
-            if token not in last_name_2_dist:
-                last_name_2_dist[token] = 1
-            else:
-                last_name_2_dist[token] += 1
-    # Add tokens from other data files
-    for line in open(first_name_file):
-        line = line.strip('\n').strip()
-        tokens = re.findall(pattern, line)
-        for token in tokens:
-            if token not in last_name_2_dist:
-                last_name_2_dist[token] = 0
-    for line in open(last_name_1_file):
-        line = line.strip('\n').strip()
-        tokens = re.findall(pattern, line)
-        for token in tokens:
-            if token not in last_name_2_dist:
-                last_name_2_dist[token] = 0
-    # Calculate probabilities
-    for key, value in last_name_2_dist.iteritems():
-        last_name_2_dist[key] = float(value) / float(token_count)
-
-    # Order dictionaries by key
-    return first_name_dist, last_name_1_dist, last_name_2_dist
+    for key, value in prob_dist.iteritems():
+        prob_dist[key] = float(value) / float(token_count)
+    return prob_dist
 
 
-# Create dictionaries of pairs token-probability from raw data
-# These dictionaries define the distribution of probability for each token
-first_name_dist, last_name_1_dist, last_name_2_dist = discrete_distributions()
+def discrete_distribution(prob_dist, tokens_1, tokens_2):
+    """
+    Add tokens from other states to a given probability distribution.
 
-# Name of the model
-model = HiddenMarkovModel(name="First-Last-Names")
+    Added tokens have probability = 0.
 
-# States of the model
-first_name = State(DiscreteDistribution(first_name_dist), name='First Name')
-last_name_1 = State(DiscreteDistribution(last_name_1_dist), name='Last Name 1')
-last_name_2 = State(DiscreteDistribution(last_name_2_dist), name='Last Name 2')
+    Parameters:
+        prob_dist (dict): probability distribution of a given state
+        tokens_1 (list): tokens of another state
+        tokens_2 (list): tokens of another state
+    Returns:
+        prob_dist (dict): input probability distribution + added tokens
 
-# Transition probabilities
-# Obtained from a huge dataset of names
-model.add_transition(model.start, first_name, 1)
-model.add_transition(first_name, first_name, 0.349495808)
-model.add_transition(first_name, last_name_1, 0.650504192)
-model.add_transition(last_name_1, last_name_1, 0.02401917)
-model.add_transition(last_name_1, last_name_2, 0.942227494)
-model.add_transition(last_name_1, model.end, 0.033753336)
-model.add_transition(last_name_2, last_name_2, 0.055919604)
-model.add_transition(last_name_2, model.end, 0.944080396)
+    """
+    # Join tokens_1 and tokens_2, without repetition
+    tokens = list(set().union(tokens_1, tokens_2))
+    # Add tokens to prob_dist with probability = 0
+    for token in tokens:
+        if token not in prob_dist:
+            prob_dist[token] = float(0)
+    return prob_dist
 
-# "Bake" the model, finalizing its structure
-model.bake(verbose=True)
 
-# Test
-observation = 'ALBERTO ALEJO DE LA CORTE WILSON'
-sequence = observation.split()
+def main():
+    """Create a Hidden Markov Model."""
+    # Name of the model
+    model = HiddenMarkovModel(name="First-Last-Names")
 
-# Probability of this sequence
-print('Observation: ' + observation)
-print('P(sequence) = ' + str(
-    math.e**model.forward(sequence)[len(sequence), model.end_index]))
+    # Extract tokens from the training sets
+    first_name_tokens = extract_tokens(first_name_file, separators)
+    last_name_1_tokens = extract_tokens(last_name_1_file, separators)
+    last_name_2_tokens = extract_tokens(last_name_2_file, separators)
 
-# Probability of token 2 = last_name_1
-print(
-    math.e**model.forward_backward(sequence)
-    [1][2, model.states.index(last_name_1)]
-)
-# Probability of token 2 = last_name_2
-print(
-    math.e**model.forward_backward(sequence)
-    [1][2, model.states.index(last_name_2)]
-)
-# Probability of token 0 = first_name
-print(
-    math.e**model.forward_backward(sequence)
-    [1][0, model.states.index(first_name)]
-)
-# Probability of the sequence given it is first_name at token 1
-print(math.e**model.backward(sequence)[1, model.states.index(first_name)])
-# Probable series of states given the above sequence
-print(' '.join(state.name for i, state in model.maximum_a_posteriori(
-    sequence)[1]))
+    # Calculate probability distributions for each token set
+    first_name_dist = probability_distribution(first_name_tokens)
+    last_name_1_dist = probability_distribution(last_name_1_tokens)
+    last_name_2_dist = probability_distribution(last_name_2_tokens)
+
+    # Calculate discrete distributions
+    first_name_dist = discrete_distribution(
+        first_name_dist, last_name_1_tokens, last_name_2_tokens
+        )
+    last_name_1_dist = discrete_distribution(
+        last_name_1_dist, first_name_tokens, last_name_2_tokens
+        )
+    last_name_2_dist = discrete_distribution(
+        last_name_2_dist, first_name_tokens, last_name_1_tokens
+        )
+
+    # States of the model
+    first_name = State(DiscreteDistribution(
+        first_name_dist), name='First Name'
+        )
+    last_name_1 = State(DiscreteDistribution(
+        last_name_1_dist), name='Last Name 1'
+        )
+    last_name_2 = State(DiscreteDistribution(
+        last_name_2_dist), name='Last Name 2'
+        )
+
+    # Transition probabilities
+    # Obtained from a huge dataset of names
+    model.add_transition(model.start, first_name, 1)
+    model.add_transition(first_name, first_name, 0.349495808)
+    model.add_transition(first_name, last_name_1, 0.650504192)
+    model.add_transition(last_name_1, last_name_1, 0.02401917)
+    model.add_transition(last_name_1, last_name_2, 0.942227494)
+    model.add_transition(last_name_1, model.end, 0.033753336)
+    model.add_transition(last_name_2, last_name_2, 0.055919604)
+    model.add_transition(last_name_2, model.end, 0.944080396)
+
+    # "Bake" the model, finalizing its structure
+    model.bake(verbose=True)
+
+    # Test
+    observation = 'ALBERTO ALEJO DE LA CORTE WILSON'
+    sequence = observation.split()
+
+    # Probability of this sequence
+    print('Observation: ' + observation)
+    print('P(sequence) = ' + str(
+        math.e**model.forward(sequence)[len(sequence), model.end_index]))
+
+    # Probability of token 2 = last_name_1
+    print(
+        math.e**model.forward_backward(sequence)
+        [1][2, model.states.index(last_name_1)]
+    )
+    # Probability of token 2 = last_name_2
+    print(
+        math.e**model.forward_backward(sequence)
+        [1][2, model.states.index(last_name_2)]
+    )
+    # Probability of token 0 = first_name
+    print(
+        math.e**model.forward_backward(sequence)
+        [1][0, model.states.index(first_name)]
+    )
+    # Probability of the sequence given it is first_name at token 1
+    print(math.e**model.backward(sequence)[1, model.states.index(first_name)])
+    # Probable series of states given the above sequence
+    print(' '.join(state.name for i, state in model.maximum_a_posteriori(
+        sequence)[1]))
+
+
+if __name__ == '__main__':
+    main()
