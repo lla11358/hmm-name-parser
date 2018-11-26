f9e2757c4efc8102f8e29edeb2419d303bf5bf13
Alberto <alberto@ayax.estudio.net>
2018-11-26 20:57:36 +0100

    Add modules tokenizer and normalizer; new transition graph

--- config.py
+++ config.py
@@ -1,12 +1,40 @@
-"""Configuration file for hmm_first_last_names script."""
+"""Configuration file for hmm_name_parser.py."""
 
-# Pattern defining the token separators
-separators = '[ ,]+'
-# Graph types: first name first or last name first
-graph_types = ['FN', 'LN']
-graph_type = graph_types[0]
+# Pattern used to extract particles and tokens
+token_pattern = \
+    '( de la | de las | de los| del | da | de | di | do | dos | el | i |' + \
+    ' la | las | los | le | san | van | y |[a-z-]+)'
+# List of particles
+particles = [
+    ' da ',
+    ' de la ',
+    ' de las ',
+    ' de los ',
+    ' del ',
+    ' de ',
+    ' di ',
+    ' do ',
+    ' dos ',
+    ' el ',
+    ' i ',
+    ' la ',
+    ' las ',
+    ' los ',
+    ' le ',
+    ' san ',
+    ' van ',
+    ' y '
+]
+# Length of subtokens
+subtoken_length = 4
+# Graph types: First Name first or Last Name first
+graph_types = ['FNF', 'LNF']
+graph_type = graph_types[1]
 # Raw data files
-first_name_file = '/home/lla11358/data/dumps/raw_nomb.txt'
-last_name_1_file = '/home/lla11358/data/dumps/raw_ape1.tsv'
-last_name_2_file = '/home/lla11358/data/dumps/raw_ape2.tsv'
-test_set_file = '/home/lla11358/data/dumps/ccss_nie.txt'
+staging_dir = '/home/alberto/data/HMM/staging/'
+input_dir = '/home/alberto/data/HMM/input/'
+output_dir = '/home/alberto/data/HMM/output/'
+fn_file = staging_dir + 'first_name.csv'
+ln1_file = staging_dir + 'last_name1.csv'
+ln2_file = staging_dir + 'last_name2.csv'
+test_set_file = input_dir + 'test_set_1.txt'
---
+++ normalizer.py
@@ -0,0 +1,16 @@
+"""
+Text normalizer.
+
+Common text normalization functions.
+
+"""
+
+import unicodedata
+
+
+def unicode(str):
+    """
+    Normalize unicode data to remove umlauts, accents etc.
+    """
+    return unicodedata.normalize(
+        'NFKD', str).encode('ASCII', 'ignore').decode('utf-8')
