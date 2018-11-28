c056fcdb4893bed730766f5c02b6090d933d2756
Alberto Lezcano Lastra <lezcano_a@cantabria.es>
2018-11-28 14:23:37 +0100

    Load data into dataframes

--- prepare_data.py
+++ prepare_data.py
@@ -7,31 +7,42 @@ Obtains discrete probability distributions from source data.
 
 """
 
-
-import config
 import pandas
-
-fn = pandas.DataFrame()
-ln = pandas.DataFrame()
+import normalizer
+import tokenizer
+import config
 
 
 def main():
     """
-    Process input data files.
+    Process data source files.
+
+    Generates a discrete distribution of probabilities for each state.
+
     """
-
     # Load csv files into dataframes.
+    list = []
     for file in config.fn_file:
-        fn.read_csv(
-            config.staging_dir + file
+        df = pandas.read_csv(
+            config.staging_dir + file,
+            sep=',',
+            encoding=config.encoding
         )
+        list.append(df)
+    first_name_df = pandas.concat(list, axis=0, ignore_index=True)
+
+    list = []
     for file in config.ln_file:
-        ln.read_csv(
-            config.staging_dir + file
+        df = pandas.read_csv(
+            config.staging_dir + file,
+            sep=',',
+            encoding=config.encoding
         )
+        list.append(df)
+    last_name_df = pandas.concat(list, axis=0, ignore_index=True)
 
+    # Extract tokens
     
-
 
 if __name__ == '__main__':
     main()
